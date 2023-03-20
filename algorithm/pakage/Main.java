a89a3384b296402151a780dc62978cf4308ea369
trieutuan22012001 <trieutuansky22012001@gmail.com>
2023-03-20 13:35:17 +0700

    implement method quick sort

--- algorithm/pakage/Main.java
+++ algorithm/pakage/Main.java
@@ -1,12 +1,18 @@
 package algorithm.pakage;
-
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args){
-        BubbleSort sorting = new BubbleSort(10);
-        System.out.println("Before sorting:");
-        sorting.prints();
-        sorting.bubblesort();
-        System.out.println("After sorting:");
-        sorting.prints();
+//        BubbleSort sorting = new BubbleSort(10);
+//        System.out.println("Before sorting:");
+//        sorting.prints();
+//        sorting.bubblesort();
+//        System.out.println("After sorting:");
+//        sorting.prints();
+
+        int[] arr = QuickSort.generate(10,0,10);
+        System.out.println("Unsorted array: " + Arrays.toString(arr));
+        QuickSort.sort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+
     }
 }
---
+++ algorithm/pakage/QuickSort.java
@@ -0,0 +1,74 @@
+package algorithm.pakage;
+
+import java.util.Random;
+
+public class QuickSort {
+
+    public static void sort(int[] numbers){
+        quicksort(numbers, 0, numbers.length -1);
+    }
+    // generate number array
+    public static int[] generate(int length, int lowindex, int highindex){
+        int[] numbers = new int[length];
+        Random random = new Random();
+        for ( int i =0; i<length; i++){
+            numbers[i] = random.nextInt(highindex - lowindex +1) + lowindex;
+        }
+        return numbers;
+    }
+    private static void quicksort(int[] array, int lowIndex, int highIndex) {
+
+        if (lowIndex >= highIndex) {
+            return;
+        }
+
+        int pivotIndex = new Random().nextInt(highIndex - lowIndex) + lowIndex;
+        int pivot = array[pivotIndex];
+        swap(array, pivotIndex, highIndex);
+
+        int leftPointer = partition(array, lowIndex, highIndex, pivot);
+
+        quicksort(array, lowIndex, leftPointer - 1);
+        quicksort(array, leftPointer + 1, highIndex);
+
+    }
+
+    private static int partition(int[] array, int lowIndex, int highIndex, int pivot) {
+        int leftPointer = lowIndex;
+        int rightPointer = highIndex - 1;
+
+        while (leftPointer < rightPointer) {
+
+            while (array[leftPointer] <= pivot && leftPointer < rightPointer) {
+                leftPointer++;
+            }
+
+            while (array[rightPointer] >= pivot && leftPointer < rightPointer) {
+                rightPointer--;
+            }
+
+            swap(array, leftPointer, rightPointer);
+        }
+
+        if(array[leftPointer] > array[highIndex]) {
+            swap(array, leftPointer, highIndex);
+        }
+        else {
+            leftPointer = highIndex;
+        }
+
+        return leftPointer;
+    }
+
+    private static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+    private static void printArray(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
